ec2f4d687aab734b0180eb67ead3358735eebaa8
Nam <75553534+namlt@users.noreply.github.com>
2020-12-31 14:56:34 +0700

    Update install.sh
    first look

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
 
+#!/bin/sh
+
+rm -f mbscript.sh
+OUTPUT=$(cat /etc/*release)
+if  echo $OUTPUT | grep -q "Ubuntu" ; then
+        echo "Dang tien hanh cap nhat cac thu vien Ubuntu va tai goi cai dat MBScript..."
+#---- Bat dau chay lenh keo cai thu vien ve----#        
+apt-get install curl wget -y 1 > /dev/null
+curl --silent -o mbscript.sh "https://linkdown.sh" 2>/dev/null
+chmod +x mbscript.sh
+else
+
+                echo -e "\nMBScript hien tai chi ho tro Ubuntu do he dieu hanh Centos se End Of Life som hon du kien.\n"
+                echo -e "\nThong tin chi tiet: https://wiki.centos.org/About/Product\n"
+                exit 1
+fi
+rm -f install.sh
+./mbscript.sh
